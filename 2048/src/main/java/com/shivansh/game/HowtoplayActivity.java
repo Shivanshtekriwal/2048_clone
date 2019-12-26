bddc7764c67c8703a8719ed9e0692b4f5f6eeba2
Shivansh <53124791+Shivanshtekriwal@users.noreply.github.com>
2019-12-26 20:43:58 +0530

    2048_clone undo button disabled

---
+++ 2048/src/main/java/com/shivansh/game/HowtoplayActivity.java
@@ -0,0 +1,13 @@
+package com.shivansh.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HowtoplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.howtoplay);
+    }
+}
